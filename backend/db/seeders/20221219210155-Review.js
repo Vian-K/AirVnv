4bbe48801ca742420dee1c0df086dd8d8eefaf5b
Vian <vian.khachatourian@gmail.com>
2022-12-19 15:59:21 -0800

    Review seed successful

--- backend/db/seeders/20221219210155-Review.js
+++ backend/db/seeders/20221219210155-Review.js
@@ -9,23 +9,40 @@ if (process.env.NODE_ENV === "production") {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    options.tableName = 'Reviews'
+    await queryInterface.bulkInsert(
+      options,
+      [
+        {
+          userId: 1,
+          spotId: 1,
+          review: "This was an awesome spot!",
+          stars: 5,
+        },
+        {
+          userId: 2,
+          spotId: 2,
+          review: "This was an awesome spot!",
+          stars: 5,
+        },
+        {
+          userId: 3,
+          spotId: 3,
+          review: "This was an awesome spot!",
+          stars: 5,
+        }
+      ], {}
+    )
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    options.tableName = "Reviews"
+    const Op = Sequelize.Op;
+    await queryInterface.bulkDelete(
+      options,
+      {
+        userId: { [Op.in]: [1, 2, 3]}
+      }
+    )
   }
 };
